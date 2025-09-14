bd595b8e197ba411dfcbda15cd299ce9658369b1
vikbro <vikso.todorov@gmail.com>
2025-09-14 14:10:05 +0300

    Added Dialogic

--- scripts/global/events.gd
+++ scripts/global/events.gd
@@ -1,11 +1,5 @@
 extends Node
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+signal on_tile_placement
+signal on_tile_removal
+signal on_tile_hover
