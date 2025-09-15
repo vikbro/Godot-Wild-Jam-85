27edf7c45d17f164da958ca0b75b9a530d4a7962
kokomosquito <detistovn@gmail.com>
2025-09-15 16:52:17 +0300

    some die

---
+++ rigid_body_3d_die.gd
@@ -0,0 +1,37 @@
+extends RigidBody3D
+
+@onready var raycasts = $RayCasts.get_children()
+
+var start_pos
+var roll_strength = 30
+
+signal roll_finished(value)
+
+func ready():
+	start_pos = global_position
+	
+func _input(event):
+	if event.is_action_pressed("ui_accept"):
+		_roll()
+
+func _roll():
+	sleeping = false
+	freeze = false
+	transform.origin = start_pos
+	linear_velocity = Vector3.ZERO
+	angular_velocity = Vector3.ZERO
+	
+	transform.basis = Basis(Vector3.RIGHT, randf_range(0, 2 * PI)) * transform.basis
+	transform.basis = Basis(Vector3.UP, randf_range(0, 2 * PI)) * transform.basis
+	transform.basis = Basis(Vector3.FORWARD, randf_range(0, 2 * PI)) * transform.basis
+
+	var throw_vector = Vector3(randf_range(-1, 1), randf_range(-1, 1), randf_range(-1, 1)). normalized()
+	angular_velocity = throw_vector * roll_strength / 2
+	apply_central_impulse(Vector3(0, 0, 0))
+
+
+func _on_sleeping_state_changed() -> void:
+	if sleeping:
+		for raycast in raycasts:
+			if raycast.is_colliding():
+				roll_finished.emit(raycast.opposite_side)
